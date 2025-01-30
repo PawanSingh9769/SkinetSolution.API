e4332163d2f11d5ec833d68b62cca8d676acc1fc
Pawan Singh <singhpawan9769@gmail.com>
2025-01-30 12:29:44 -0600

    Added ProductController

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions option) : DbContext(option)
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+    }
+
+   
+    
+    
+}
