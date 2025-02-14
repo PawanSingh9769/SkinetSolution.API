6f29656a39d2a77a1e14713760d136dea1b32c94
Pawan Singh <singhpawan9769@gmail.com>
2025-02-14 11:25:03 -0600

    commited Infrastructure and core as well

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IReadOnlyList<Product>> GetProductAsync(string? brands, string? types,string? sort); // passing the parameter for filtering 
+        Task<Product?> GetProductByIdAsync(int id);  //"?" this if the product exists or not 
+
+        Task<IReadOnlyList<string>> GetBrandsAsync();
+        Task<IReadOnlyList<string>> GetTypeAsync();
+        void AddProduct(Product product); // void is used because it does not directly contacted database That is why is "sync"
+        void UpdateProduct(Product product);    
+        void DeleteProduct(Product product);
+
+        bool ProductExists(int id);
+        Task<bool> SaveChangesAsync(); // if anything change it return true
+
+
+    }
+}
