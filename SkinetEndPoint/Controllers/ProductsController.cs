3418b487854a01aef5b5644d219f9d4a3b1964e5
Pawan Singh <singhpawan9769@gmail.com>
2025-02-14 11:18:45 -0600

    Implemented Repository Pattern to project

--- SkinetEndPoint/Controllers/ProductsController.cs
+++ SkinetEndPoint/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,26 +8,23 @@ namespace SkinetEndPoint.Controllers
 {
     [Route("api/products")]
     [ApiController]
-    public class ProductsController : Controller
+    public class ProductsController(IProductRepository repo) : Controller
     {
-        private readonly StoreContext context;
-        public ProductsController(StoreContext context)
-        {
-            this.context = context;
-        }
+        //we use api controller because then we don't need to specify controller where to look for the attribute e.g [fromBody], [FromQuey] etc
+       
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            return await context.Products.ToListAsync();
+            return Ok(await repo.GetProductAsync(brand, type,sort));
         }
 
 
         [HttpGet("{id:int}")] // api/products/2
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await context.Products.FindAsync(id);
+            var product = await repo.GetProductByIdAsync(id);
             if (product == null) return NotFound();
             return product;
         }
@@ -36,9 +34,13 @@ namespace SkinetEndPoint.Controllers
         [HttpPost]
         public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
-            context.Products.Add(product);
-            await context.SaveChangesAsync();
-            return product;
+            repo.AddProduct(product);
+            if(await repo.SaveChangesAsync())
+            {
+                return CreatedAtAction("GetProduct",new {id = product.Id});
+            }
+           
+            return BadRequest("Problem creating product");
         }
 
 
@@ -47,26 +49,46 @@ namespace SkinetEndPoint.Controllers
         {
             if (product.Id != id || !ProductExists(id))
                 return BadRequest("Cannot update this product");
-            context.Entry(product).State = EntityState.Modified;
-            await context.SaveChangesAsync();
-            return NoContent();
+            repo.UpdateProduct(product);
+            if (await repo.SaveChangesAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest("Problem Updating product");
         }
 
+
+        //changes left
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
-            var product = await context.Products.FindAsync(id);
+            var product = await repo.GetProductByIdAsync(id);
             if (product == null) return NotFound();
-            context.Products.Remove(product);
-            await context.SaveChangesAsync();
-            return NoContent();
+            repo.DeleteProduct(product);
+            if (await repo.SaveChangesAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest("Problem Deleting the product");
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+        {
+            return Ok(await repo.GetBrandsAsync());
         }
 
 
+        [HttpGet("Types")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+        {
+            return Ok(await repo.GetTypeAsync());
+        }
+
         private bool ProductExists(int id)
         {
-            return context.Products.Any(x => x.Id == id);
+            return repo.ProductExists(id);
         }
     }
 }
--- SkinetEndPoint/Program.cs
+++ SkinetEndPoint/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();  // using this the service is now available to use in controller class
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -25,5 +28,24 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(System.Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+
+}
+
+
 
 app.Run();
